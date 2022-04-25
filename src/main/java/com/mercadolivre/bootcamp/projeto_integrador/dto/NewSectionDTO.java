2fbb6c0577634f796a0a38783010393e33c7a778
fgnanni <flavio.nanni@mercadolivre.com>
2022-04-25 18:37:16 -0300

    Exclude method not used

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewSectionDTO.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewSectionDTO.java
@@ -3,8 +3,10 @@ package com.mercadolivre.bootcamp.projeto_integrador.dto;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.InBoundOrder;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Section;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -14,42 +16,21 @@ import java.util.stream.Collectors;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NewSectionDTO {
 
-    private String sectionId;
-    private String type;
     private BigDecimal capacity;
     private float currentTemperature;
-    private List<InBoundOrder> listInBoundOrder;
     private Category category;
     private String warehouseId;
 
-    private Section section;
-
-    public NewSectionDTO(String sectionId, String type, BigDecimal capacity,
-                         float currentTemperature, Category category, String warehouseId) {
-
-        this.sectionId = sectionId;
-        this.type = type;
-        this.capacity = capacity;
-        this.currentTemperature = currentTemperature;
-        this.listInBoundOrder = new ArrayList<>();
-        this.category = category;
-        this.warehouseId = warehouseId;
-    }
-
     public static Section convert(NewSectionDTO newSectionDTO){
         return Section.builder()
-                .sectionId(newSectionDTO.getSectionId())
-                .type(newSectionDTO.getType())
                 .capacity(newSectionDTO.getCapacity())
                 .currentTemperature(newSectionDTO.getCurrentTemperature())
-                .listInBoundOrder(newSectionDTO.getListInBoundOrder())
                 .category(newSectionDTO.getCategory())
                 .warehouseId(newSectionDTO.getWarehouseId()).build();
     }
 
-    public static List<Section> convert(List<NewSectionDTO> newSectionDTOList){
-        return newSectionDTOList.stream().map(NewSectionDTO::convert).collect(Collectors.toList());
-    }
 }
