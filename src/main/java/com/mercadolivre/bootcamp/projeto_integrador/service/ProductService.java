55ec555a06cf3f3ada2bf9c2ddcd4a1922312679
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-25 12:45:42 -0300

    Added methods to ProductService and ProductServiceImpl

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/ProductService.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/ProductService.java
@@ -1,4 +1,13 @@
 package com.mercadolivre.bootcamp.projeto_integrador.service;
 
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    Product find(String productName);
+    Product find(Category category);
+    List<Product> findAll(String salesmanId);
+    List<Product> findAll();
 }
--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/ProductServiceImpl.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/ProductServiceImpl.java
@@ -1,8 +1,36 @@
 package com.mercadolivre.bootcamp.projeto_integrador.service;
 
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Product;
+import com.mercadolivre.bootcamp.projeto_integrador.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
+    private final ProductRepository productRepository;
+
+    @Override
+    public Product find(String productName) {
+        return productRepository.findByProductName(productName).orElse(null);
+    }
+
+    @Override
+    public Product find(Category category) {
+        return productRepository.findByCategory(category).orElse(null);
+    }
+
+    @Override
+    public List<Product> findAll(String salesmanId) {
+        return productRepository.findAllBySalesman_Id(salesmanId);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
 }
