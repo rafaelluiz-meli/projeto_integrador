ecc0cd7066f1012eb4e60af286b22b1e39819f59
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-25 17:35:10 -0300

    Added new method to ProductService and added Create method

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/ProductService.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/ProductService.java
@@ -1,13 +1,17 @@
 package com.mercadolivre.bootcamp.projeto_integrador.service;
 
+import com.mercadolivre.bootcamp.projeto_integrador.dto.NewProductDto;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
+    Product create(NewProductDto newProductDto);
+    void delete(String id);
+
     Product find(String productName);
-    Product find(Category category);
+    List<Product> find(Category category);
     List<Product> findAll(String salesmanId);
     List<Product> findAll();
 }
