f6ad9d63652131ab0314f5583eae59ecfec16225
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-25 17:37:27 -0300

    Added new tests to ProductServiceTest

--- src/test/java/com/mercadolivre/bootcamp/projeto_integrador/unit/ProductServiceTest.java
+++ src/test/java/com/mercadolivre/bootcamp/projeto_integrador/unit/ProductServiceTest.java
@@ -1,15 +1,27 @@
 package com.mercadolivre.bootcamp.projeto_integrador.unit;
 
+import com.mercadolivre.bootcamp.projeto_integrador.dto.NewProductDto;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Product;
+import com.mercadolivre.bootcamp.projeto_integrador.exception.InvalidProductException;
 import com.mercadolivre.bootcamp.projeto_integrador.repository.ProductRepository;
-import com.mercadolivre.bootcamp.projeto_integrador.service.ProductService;
+import com.mercadolivre.bootcamp.projeto_integrador.service.ProductServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
@@ -18,22 +30,121 @@ public class ProductServiceTest {
     private ProductRepository productRepository;
 
     @InjectMocks
-    private ProductService productService;
+    private ProductServiceImpl productService;
 
+    @Test
+    @DisplayName("it should find a product by name")
     public void shouldFindProductByName() {
-        // TODO: 25/04/22
+        // Arrange tests
+        Product product1 = Product.builder()
+                                .productName("Product")
+                                .build();
+
+        // Execute action
+        Mockito.when(productRepository.findByProductName(any())).thenReturn(Optional.of(product1));
+        Product result = productService.find("Product");
+
+        // Assert result
+        assertEquals(product1, result);
     }
 
+    @Test
+    @DisplayName("it should find a product by category")
     public void shouldFindProductByCategory() {
-        // TODO: 25/04/22  
-    }
-    
-    public void shouldFindProductListBySalesmanId() {
-        // TODO: 25/04/22  
-    }
-    
-    public void shouldFindProductList() {
-        // TODO: 25/04/22  
+        // Arrange tests
+        Product product = Product.builder().category(any()).build();
+        List<Product> productList = Arrays.asList(product, product);
+
+        // Execute action
+        Mockito.when(productRepository.findAllByCategory(Category.FRESH)).thenReturn(productList);
+        List<Product> resultList = productService.find(Category.FRESH);
+
+        // Assert result
+        assertEquals(productList, resultList);
     }
 
+    @Test
+    @DisplayName("it should find a product list by salesman id")
+    public void shouldFindProductListBySalesmanId() {
+        // Arrange tests
+        Product product = Product.builder().salesman(any()).build();
+        List<Product> productList = Arrays.asList(product, product);
+
+        // Execute action
+        Mockito.when(productRepository.findAllBySalesman_Id("any")).thenReturn(productList);
+        List<Product> resultList = productService.findAll(any());
+
+        // Assert result
+        assertEquals(resultList, productList);
+    }
+
+    @Test
+    @DisplayName("it should find a product list")
+    public void shouldFindProductList() {
+
+        // Arrange tests
+        Product product = Product.builder().productName("Product 1").build();
+        List<Product> productList = Arrays.asList(product, product);
+
+        // Execute action
+        Mockito.when(productRepository.findAll()).thenReturn(productList);
+        List<Product> resultList = productService.findAll();
+
+        // Assert result
+        assertEquals(resultList, productList);
+    }
+
+    @Test
+    @DisplayName("it should create a new product")
+    public void shouldCreateProduct() {
+        // Arrange tests
+        NewProductDto productDto = NewProductDto.builder()
+                .productName("Product")
+                .category(Category.FRESH)
+                .maxTemperature(1F)
+                .minimumTemperature(0F)
+                .salesman_id("Salesman ID")
+                .volume(BigDecimal.valueOf(10))
+                .build();
+
+        Product product = Product.builder()
+                .id("id")
+                .productName("Product")
+                .category(Category.FRESH)
+                .maxTemperature(1F)
+                .minimumTemperature(0F)
+                .volume(BigDecimal.valueOf(10))
+                .build();
+        // Execute action
+        Mockito.when(productRepository.save(any())).thenReturn(product);
+        Product result = productService.create(productDto);
+
+        // Assert result
+        assertEquals(product, result);
+    }
+
+    @Test
+    @DisplayName("it should delete an existing product")
+    public void shouldDeleteProduct() {
+        // Arrange tests
+        Product product = Product.builder().id("id").build();
+        // Execute action
+        Mockito.when(productRepository.findById(any())).thenReturn(Optional.of(product));
+        doNothing().when(productRepository).delete(any());
+        // Assert result
+        assertDoesNotThrow(() -> {
+            productService.delete("id");
+        });
+    }
+
+    @Test
+    @DisplayName("it should throw an exception if attempting to delete a nonexistent product")
+    public void shouldNotDeleteNonExistentProduct() {
+        // Arrange tests
+
+        // Execute action
+
+        // Assert result
+        assertThrows(InvalidProductException.class, () -> productService.delete(any()));
+    }
 }
