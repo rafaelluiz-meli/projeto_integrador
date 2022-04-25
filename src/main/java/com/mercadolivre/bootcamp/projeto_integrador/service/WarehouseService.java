217baa5e28522ed19ce0e6e1099063cce2fb2756
hugo <hugo.cruz@mercadolivre.com>
2022-04-25 14:47:06 -0300

    added service methods

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/WarehouseService.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/WarehouseService.java
@@ -1,6 +1,12 @@
 package com.mercadolivre.bootcamp.projeto_integrador.service;
 
 
-public interface WarehouseService {
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Warehouse;
 
+import java.util.List;
+
+public interface WarehouseService {
+    Warehouse find(String warehouseId);
+    List<Warehouse> findAll();
+    boolean isValidWarehouse(String warehouseId);
 }
--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/WarehouseServiceImpl.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/WarehouseServiceImpl.java
@@ -1,10 +1,44 @@
 package com.mercadolivre.bootcamp.projeto_integrador.service;
 
 
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Warehouse;
+import com.mercadolivre.bootcamp.projeto_integrador.exception.WarehouseNaoExisteException;
 import com.mercadolivre.bootcamp.projeto_integrador.repository.WarehouseRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WarehouseServiceImpl implements WarehouseService{
     private WarehouseRepository warehouseRepository;
+
+
+    @Override
+    public Warehouse find(String warehouseId) {
+        Optional<Warehouse> wh = warehouseRepository.findById(warehouseId);
+        if(wh.isEmpty()){
+            throw new WarehouseNaoExisteException(warehouseId);
+        }else{
+            return wh.get();
+        }
+    }
+
+    @Override
+    public List<Warehouse> findAll() {
+        List<Warehouse> warehouses = warehouseRepository.findAll();
+        return warehouses;
+    }
+
+    @Override
+    public boolean isValidWarehouse(String warehouseId) {
+        Optional<Warehouse> wh = warehouseRepository.findById(warehouseId);
+        if(wh.isEmpty()){
+            throw new WarehouseNaoExisteException(warehouseId);
+        }else{
+            return true;
+        }
+    }
+
+
 }
