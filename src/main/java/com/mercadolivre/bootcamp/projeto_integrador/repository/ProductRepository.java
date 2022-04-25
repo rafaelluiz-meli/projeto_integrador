ce8998adf1e7e20f6f67e320d5456ae08fded9fa
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-25 12:44:52 -0300

    Added new methods to ProductRepository

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/ProductRepository.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.mercadolivre.bootcamp.projeto_integrador.repository;
 
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByProductName(String productName);
+    Optional<Product> findByCategory(Category category);
+    List<Product> findAllBySalesman_Id(String salesmanId);
 }
