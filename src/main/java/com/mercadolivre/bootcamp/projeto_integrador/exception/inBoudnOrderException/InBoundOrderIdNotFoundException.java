f34047c21327467ba1d5496b10c71fce6342a6c3
victosantos <victor.vsantos@mercadolivre.com>
2022-04-26 16:38:22 -0300

    create InBoundOrderEmptyListException

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/exception/inBoudnOrderException/InBoundOrderIdNotFoundException.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/exception/inBoudnOrderException/InBoundOrderIdNotFoundException.java
@@ -1,9 +1,10 @@
-package com.mercadolivre.bootcamp.projeto_integrador.exception;
+package com.mercadolivre.bootcamp.projeto_integrador.exception.inBoudnOrderException;
 
+import com.mercadolivre.bootcamp.projeto_integrador.exception.BaseException;
 import org.springframework.http.HttpStatus;
 import java.time.ZonedDateTime;
 
-public class InBoundOrderIdNotFoundException extends BaseException{
+public class InBoundOrderIdNotFoundException extends BaseException {
 
     public InBoundOrderIdNotFoundException(Long inBoundOrderNumber){
         super("O id " + inBoundOrderNumber + " não foi encontrado.", HttpStatus.NOT_FOUND, ZonedDateTime.now());
--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/InBoundOrderServiceImpl.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/InBoundOrderServiceImpl.java
@@ -1,7 +1,8 @@
 package com.mercadolivre.bootcamp.projeto_integrador.service;
 
 import com.mercadolivre.bootcamp.projeto_integrador.dto.NewInBoundOrderDTO;
-import com.mercadolivre.bootcamp.projeto_integrador.exception.InBoundOrderIdNotFoundException;
+import com.mercadolivre.bootcamp.projeto_integrador.exception.inBoudnOrderException.InBoundOrderEmptyListException;
+import com.mercadolivre.bootcamp.projeto_integrador.exception.inBoudnOrderException.InBoundOrderIdNotFoundException;
 import com.mercadolivre.bootcamp.projeto_integrador.repository.InBoundOrderRepository;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.InBoundOrder;
 import lombok.AllArgsConstructor;
@@ -12,38 +13,35 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class InBoundOrderServiceImpl implements InBoundOrderService{
-    private InBoundOrderRepository inBoundOrderrepository;
+    private final InBoundOrderRepository inBoundOrderrepository;
 
     @Override
     public InBoundOrder addInBoundOrder(NewInBoundOrderDTO inBoundOrderDTO){
         //Todo: validation of section, warehouse and representative
-        InBoundOrder savedInBoundOrderDTO = inBoundOrderrepository.save(NewInBoundOrderDTO.convert(inBoundOrderDTO));
-        return savedInBoundOrderDTO;
+        return inBoundOrderrepository.save(NewInBoundOrderDTO.convert(inBoundOrderDTO));
     }
 
     @Override
     public List<InBoundOrder> getAllInBoundOrder(){
-        List<InBoundOrder> listInBoundOrder = inBoundOrderrepository.findAll();
-        return listInBoundOrder;
+        return inBoundOrderrepository.findAll();
     }
 
     @Override
     public InBoundOrder getInBoundOrderById(Long inBoundOrderNumber){
-        InBoundOrder getInBoundOrderById = inBoundOrderrepository.findById(inBoundOrderNumber).orElseThrow(() ->
+        return inBoundOrderrepository.findById(inBoundOrderNumber).orElseThrow(() ->
                 new InBoundOrderIdNotFoundException(inBoundOrderNumber));
-        return getInBoundOrderById;
     }
 
     @Override
     public InBoundOrder updateInBoundOrder(InBoundOrder inBoundOrder){
-        InBoundOrder getInBoundOrderById = inBoundOrderrepository.findById(inBoundOrder.getInBoundOrderNumber()).orElseThrow(() ->
+        InBoundOrder updateInBoundOrderById = inBoundOrderrepository.findById(inBoundOrder.getInBoundOrderNumber()).orElseThrow(() ->
                 new InBoundOrderIdNotFoundException(inBoundOrder.getInBoundOrderNumber()));
 
-        getInBoundOrderById.setInBoundOrderDate(inBoundOrder.getInBoundOrderDate());
+        updateInBoundOrderById.setInBoundOrderDate(inBoundOrder.getInBoundOrderDate());
         //Todo: finish implementation of commented lines below
 //        getInBoundOrderById.setBatchStockList(inBoundOrder.getBatchStockList());
 //        getInBoundOrderById.setRepresentativeId(inBoundOrder.getRepresentativeId());
-        return inBoundOrderrepository.save(getInBoundOrderById);
+        return inBoundOrderrepository.save(updateInBoundOrderById);
     }
 
     @Override
