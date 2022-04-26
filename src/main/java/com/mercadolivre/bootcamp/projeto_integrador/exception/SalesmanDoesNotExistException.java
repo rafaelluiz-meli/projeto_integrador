f3f2f90f09f961f92fdc862c389dc60942f1b146
josgabri <josgabri@student.42sp.org.br>
2022-04-26 10:51:38 -0300

    exception is alredy done

---
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/exception/SalesmanDoesNotExistException.java
@@ -0,0 +1,6 @@
+package com.mercadolivre.bootcamp.projeto_integrador.exception;
+
+public class SalesmanDoesNotExistException extends Throwable {
+    public SalesmanDoesNotExistException(String s) {
+    }
+}
