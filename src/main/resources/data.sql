b7e5a46af379f7111fdff9e8e5b2c45f82bfc66e
fgnanni <flavio.nanni@mercadolivre.com>
2022-04-27 11:28:02 -0300

    Add file data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+--Popula tabela Salesman
+INSERT INTO Salesman (fullName) VALUES ('Rafael'), ('Fernando'), ('Vinicius'), ('Alan'), ('Kleber'), ('Romário'), ('Ronaldo');
+--Popula
+INSERT INTO Product (productName, volume, minimumTemperature, maxTemperature, category, salesman_id) VALUES ('banana', 10, 5, 15)--Popula tabela Salesman
+    INSERT INTO Salesman (fullName) VALUES ('Rafael'), ('Fernando'), ('Vinicius'), ('Alan'), ('Kleber'), ('Romário'), ('Ronaldo');
