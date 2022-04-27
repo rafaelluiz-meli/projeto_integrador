406ba6ba375d8dc83d2ed1c263ddbdb46ac73d18
fgnanni <flavio.nanni@mercadolivre.com>
2022-04-27 19:30:38 -0300

    Config application.properties

--- src/main/resources/data-postegresql.sql
+++ src/main/resources/data-postegresql.sql
@@ -30,9 +30,10 @@ INSERT INTO Product (productName, volume, minimumTemperature, maxTemperature, ca
                                                                                                             ('Peito de Peru', 100, 10, 15,'Fresco',7),
                                                                                                             ('Mortadela', 100, 10, 15,'Fresco',7);
 
-INSERT INTO BatchStock (initialQuantity, currentQuantity, price, dueDate, manufacturingDate, manufacturingTime, product_id) VALUES  (400, 400, '2022/01/15', '23:00:00', 1),
-                                                                                                                                    (1000, 1000, '2022/01/16', '14:30:00', 3),
-                                                                                                                                    (2000, 2000, '2022/05/15', '14:30:00', 2),
-                                                                                                                                    (250, 250, '2022/05/21', '17:45:00', 5),
-                                                                                                                                    (750, 250, '2022/07/03', '14:35:00', 7);
-
+INSERT INTO batch_stock (initial_quantity, current_quantity, price, due_date, manufacturing_date, manufacturing_time,
+                         product_id)
+VALUES ( 400, 400, 10, '2022/01/15','2021/01/10' ,'1999-01-08 23:00:00', 1),
+       (1000, 1000, 20, '2022/01/16', '2022/01/13' ,'1999-01-08 14:30:00', 3),
+       (2000, 2000, 30, '2022/05/15', '2022/05/10', '1999-01-08 14:30:00', 2),
+       (250, 250, 40, '2022/05/21', '2022/05/10', '1999-01-08 17:45:00', 5),
+       (750, 250, 50, '2022/07/03', '2022/07/01', '1999-01-08 14:35:00', 7);
