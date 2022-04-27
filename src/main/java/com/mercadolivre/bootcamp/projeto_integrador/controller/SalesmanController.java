08a81c975b1f52558aa2ac31d2d85af70ebf7097
marianna-meli <marianna.rodrigues@mercadolivre.com>
2022-04-27 17:04:30 -0300

    creating SalesmanController

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/SalesmanController.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/SalesmanController.java
@@ -1,7 +1,18 @@
 package com.mercadolivre.bootcamp.projeto_integrador.controller;
 
+import com.mercadolivre.bootcamp.projeto_integrador.service.SalesmanService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class SalesmanController {
+
+    private final SalesmanService salesmanService;
+
+
+
 }
+
