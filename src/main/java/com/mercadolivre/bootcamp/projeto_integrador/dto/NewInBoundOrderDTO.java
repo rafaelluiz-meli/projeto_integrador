5dde96b9b1d9f981864cf579948ae80d966fc572
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-27 15:33:05 -0300

    added requested changes

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewInBoundOrderDTO.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewInBoundOrderDTO.java
@@ -1,15 +1,13 @@
 package com.mercadolivre.bootcamp.projeto_integrador.dto;
 
 import com.mercadolivre.bootcamp.projeto_integrador.entity.BatchStock;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class NewInBoundOrderDTO {
     private String representativeId;
     private SectionDTO sectionDTO;
