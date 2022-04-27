f7f39536f392261053dd48df047d8add5a97b171
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-27 14:22:04 -0300

    Removed unused imports at NewInBoundOrderDTO

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewInBoundOrderDTO.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewInBoundOrderDTO.java
@@ -1,12 +1,10 @@
 package com.mercadolivre.bootcamp.projeto_integrador.dto;
 
 import com.mercadolivre.bootcamp.projeto_integrador.entity.BatchStock;
-import com.mercadolivre.bootcamp.projeto_integrador.entity.InBoundOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDate;
 
 @Builder
 @AllArgsConstructor
