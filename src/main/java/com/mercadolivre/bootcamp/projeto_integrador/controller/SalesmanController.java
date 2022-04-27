7d66c195c9785ce16c52d283c361bf0d1c15182f
marianna-meli <marianna.rodrigues@mercadolivre.com>
2022-04-27 19:45:53 -0300

    updating SalesmanController

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/SalesmanController.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/SalesmanController.java
@@ -1,6 +1,7 @@
 package com.mercadolivre.bootcamp.projeto_integrador.controller;
 
-import com.mercadolivre.bootcamp.projeto_integrador.dto.NewSalesmanDto;
+import com.mercadolivre.bootcamp.projeto_integrador.dto.salesmanDTO.NewSalesmanDto;
+import com.mercadolivre.bootcamp.projeto_integrador.dto.salesmanDTO.UpdateSalesmanDTO;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Salesman;
 import com.mercadolivre.bootcamp.projeto_integrador.service.SalesmanService;
 import lombok.AllArgsConstructor;
@@ -8,33 +9,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @RestController
 @AllArgsConstructor
-@RequestMapping()
+@RequestMapping("api/v1/fresh-products/salesman")
 public class SalesmanController {
 
     private final SalesmanService salesmanService;
 
-    @PostMapping("/salesman")
-    public ResponseEntity<NewSalesmanDto> createSalesman(@Valid @RequestBody Salesman salesman) {
+    @PostMapping("/addSalesman")
+    public ResponseEntity<NewSalesmanDto> createSalesman(@RequestBody Salesman salesman) {
         salesmanService.createSalesman(salesman);
-        return ResponseEntity.status(HttpStatus.CREATED).body(NewSalesmanDto.builder().build());
+        return ResponseEntity.status(HttpStatus.CREATED).body(NewSalesmanDto.map(salesman));
     }
 
-    @PutMapping("/salesman/{id}")
-    public ResponseEntity<Long> updateSalesman(@PathVariable(value = "id" ) Long salesmanId, @Valid @RequestBody Salesman salesman) {
-        Salesman salesmanAux = salesmanService.updateSalesman(salesmanId, salesman);
-        return ResponseEntity.ok().body(salesmanId);
+    @PutMapping("/updateSalesman")
+    public ResponseEntity<UpdateSalesmanDTO> updateSalesman(@RequestBody UpdateSalesmanDTO updateSalesmanDTO) {
+        Salesman salesman = updateSalesmanDTO.map();
+        Salesman updateSalesman = salesmanService.updateSalesman(salesman);
+        return ResponseEntity.ok(updateSalesmanDTO.map(updateSalesman));
     }
 
-    @DeleteMapping("/salesman/{id}")
-    public ResponseEntity<Long> deleteSalesman(@PathVariable(value = "id") Long salesmanId) {
-        salesmanService.removeSalesman(salesmanId);
-        return ResponseEntity.ok(salesmanId);
+    @DeleteMapping("/{salesman}")
+    public ResponseEntity<Long> deleteSalesman(@PathVariable(value = "salesman") Salesman salesman) {
+        salesmanService.removeSalesman(salesman.getId());
+        return ResponseEntity.ok(salesman.getId());
     }
-
-
 }
 
