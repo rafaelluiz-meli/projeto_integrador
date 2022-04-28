f159cf3199d8b570e1aff7d0c2341d75b136d5bc
fgnanni <flavio.nanni@mercadolivre.com>
2022-04-28 14:28:05 -0300

    Added endpoints to product controller

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/ProductController.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/ProductController.java
@@ -1,12 +1,41 @@
 package com.mercadolivre.bootcamp.projeto_integrador.controller;
 
+import com.mercadolivre.bootcamp.projeto_integrador.dto.NewProductDto;
+import com.mercadolivre.bootcamp.projeto_integrador.dto.ProductDto;
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Product;
 import com.mercadolivre.bootcamp.projeto_integrador.service.ProductService;
 import com.mercadolivre.bootcamp.projeto_integrador.service.ProductServiceImpl;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1/fresh-products")
 public class ProductController {
     private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody NewProductDto productDto) {
+        ProductDto createdProduct = ProductDto.convert(productService.create(productDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts(){
+        List<ProductDto> productList = ProductDto.convert(productService.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(productList);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<ProductDto>> getProductsByCategory(@RequestParam Category category) {
+        List<ProductDto> productList = ProductDto.convert(productService.findAllByCategory(category));
+        return ResponseEntity.status(HttpStatus.OK).body(productList);
+    }
+
 }
