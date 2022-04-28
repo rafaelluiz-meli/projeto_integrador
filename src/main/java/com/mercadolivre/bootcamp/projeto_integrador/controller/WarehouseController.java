af83d3eb77929b91677200f475c4a2af1cf1c000
hugo <hugo.cruz@mercadolivre.com>
2022-04-28 14:11:21 -0300

    fields with autowrite cannot be initialized with final

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/WarehouseController.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/WarehouseController.java
@@ -7,6 +7,7 @@ import com.mercadolivre.bootcamp.projeto_integrador.exception.WarehouseDoesntExi
 import com.mercadolivre.bootcamp.projeto_integrador.service.WarehouseService;
 import com.mercadolivre.bootcamp.projeto_integrador.service.WarehouseServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 public class WarehouseController {
-    private final WarehouseServiceImpl warehouseService;
+    private WarehouseService warehouseService;
     @RequestMapping("api/v1/fresh-products/warehouse")
 
 
--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/WarehouseServiceImpl.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/WarehouseServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 @Service
 public class WarehouseServiceImpl implements WarehouseService{
 
-    private final WarehouseRepository warehouseRepository;
+    private WarehouseRepository warehouseRepository;
 
     @Override
     public Warehouse findById(Long warehouseId) {
