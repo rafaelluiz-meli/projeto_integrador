4d7d91eb659174040c30f767b9643e3fe22319f4
victosantos <victor.vsantos@mercadolivre.com>
2022-04-28 15:54:57 -0300

    add properties in dto

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewPurchaseOrderDTO.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/NewPurchaseOrderDTO.java
@@ -2,10 +2,12 @@ package com.mercadolivre.bootcamp.projeto_integrador.dto;
 
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Buyer;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.PurchaseOrder;
+import com.mercadolivre.bootcamp.projeto_integrador.entity.PurchaseOrderItems;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.StatusOrder;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Builder
 @Getter
@@ -14,4 +16,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class NewPurchaseOrderDTO {
     private BuyerDTO buyerDTO;
+    private List<PurchaseOrderItems> purchaseOrderItemsList;
+    private StatusOrder statusOrder = StatusOrder.CART;
+    private LocalDate purchaseOrderDate;
 }
