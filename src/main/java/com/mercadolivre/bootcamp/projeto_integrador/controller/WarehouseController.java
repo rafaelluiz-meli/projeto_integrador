ef0472534be21130b2effdbd4de78696e49df3fe
hugo <hugo.cruz@mercadolivre.com>
2022-04-28 15:53:59 -0300

    resolver merge conflicts

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/WarehouseController.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/controller/WarehouseController.java
@@ -1,18 +1,14 @@
 package com.mercadolivre.bootcamp.projeto_integrador.controller;
 
 
-import com.mercadolivre.bootcamp.projeto_integrador.entity.Representative;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.Warehouse;
 import com.mercadolivre.bootcamp.projeto_integrador.exception.WarehouseDoesntExistException;
+import com.mercadolivre.bootcamp.projeto_integrador.exception.generics.IdNotFoundException;
 import com.mercadolivre.bootcamp.projeto_integrador.service.WarehouseService;
-import com.mercadolivre.bootcamp.projeto_integrador.service.WarehouseServiceImpl;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import javax.validation.Valid;
-import java.util.List;
 
 
 @AllArgsConstructor
@@ -34,8 +30,8 @@ public class WarehouseController {
         try{
             warehouseService.delete(id);
             return ResponseEntity.ok().body("Deleted with Success");
-        }catch(WarehouseDoesntExistException ex){
-            return new ResponseEntity("Id not found",HttpStatus.NOT_FOUND);
+        }catch(IdNotFoundException ex){
+            return new ResponseEntity(ex.getMessage(),HttpStatus.NOT_FOUND);
         }
 
     }
