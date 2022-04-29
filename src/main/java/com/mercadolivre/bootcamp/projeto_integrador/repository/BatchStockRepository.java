45ff515055d4751a63cc82641e627dfa13e75d2e
fgnanni <flavio.nanni@mercadolivre.com>
2022-04-28 23:05:48 -0300

    added new methods for filtred productId and dueDate

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/BatchStockRepository.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/BatchStockRepository.java
@@ -1,11 +1,15 @@
 package com.mercadolivre.bootcamp.projeto_integrador.repository;
 
 import com.mercadolivre.bootcamp.projeto_integrador.entity.BatchStock;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BatchStockRepository extends JpaRepository<BatchStock, Long> {
 
     List<BatchStock> findAllByProduct_Id(Long productId);
+    List<BatchStock> findAllByDueDate(LocalDate dueDate);
+    List<BatchStock> findAllByProduct_IdAndAndDueDate(Long productId, LocalDate dueDate);
 }
