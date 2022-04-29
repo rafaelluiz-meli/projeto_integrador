f0859c5057bc178b6d388565b5755093c448f996
Rafael Santana <rafael.santana@mercadolivre.com>
2022-04-29 17:55:49 -0300

    Create methods for validations

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/factory/PurchaseOrderFactory.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/factory/PurchaseOrderFactory.java
@@ -1,4 +1,33 @@
 package com.mercadolivre.bootcamp.projeto_integrador.factory;
 
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Buyer;
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Product;
+import com.mercadolivre.bootcamp.projeto_integrador.service.BatchStockService;
+import com.mercadolivre.bootcamp.projeto_integrador.service.BuyerService;
+import com.mercadolivre.bootcamp.projeto_integrador.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
 public class PurchaseOrderFactory {
+
+    private final BuyerService buyerService;
+
+    private final ProductService productService;
+
+    private final BatchStockService batchStockService;
+
+    public boolean isBuyerRegistered(Long buyerId){
+        Long result = buyerService.findById(buyerId).getBuyerId();
+        return result > 1;
+    }
+
+    public boolean isProductInStock(Long productId, Integer quantity) {
+        return productService.availableStockQuantity(quantity);
+    }
+
+    public boolean isProductValidForNextThreeWeeks(Long productId){
+        return productService.validateProductDueDate(productId);
+    }
 }
