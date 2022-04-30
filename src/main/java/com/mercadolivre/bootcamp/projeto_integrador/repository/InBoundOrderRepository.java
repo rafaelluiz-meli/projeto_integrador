dda91aa509743e10fde7812338dc30c2a4d3c064
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-30 12:46:06 -0300

    removed useless comments

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/InBoundOrderRepository.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/InBoundOrderRepository.java
@@ -9,19 +9,5 @@ import java.util.List;
 
 @Repository
 public interface InBoundOrderRepository extends JpaRepository<InBoundOrder, Long> {
-    @Query("select i from InBoundOrder i where i.batchStock.product.id = ?1")
-
-
-    List<InBoundOrder> findByBatchStock_Product_Id(Long id);
-    /*
-    List<result> resultadoFodaPraCaralho = LISTA TOPPER
-
-    resultadoDaQuery.forEach(
-     () -> {
-        // filtrar por warehouse
-        // adicionar ao DTO de resultado a warehouse e a quantidade
-     }
-    )
-     */
 
 }
