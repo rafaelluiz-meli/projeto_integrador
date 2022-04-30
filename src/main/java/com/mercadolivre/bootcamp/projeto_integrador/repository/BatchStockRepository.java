3f364f9b51261d6212b98e8d6ce5f0068470ec46
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-04-30 20:01:38 -0300

    Added new methods at BatchStockRepository

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/BatchStockRepository.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/repository/BatchStockRepository.java
@@ -1,11 +1,18 @@
 package com.mercadolivre.bootcamp.projeto_integrador.repository;
 
 import com.mercadolivre.bootcamp.projeto_integrador.entity.BatchStock;
+import com.mercadolivre.bootcamp.projeto_integrador.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BatchStockRepository extends JpaRepository<BatchStock, Long> {
 
     List<BatchStock> findAllByProduct_Id(Long productId);
+
+    List<BatchStock> findByDueDateIsLessThanEqualAndSection_SectionId(LocalDate dueDate, Long sectionId);
+
+    List<BatchStock> findByDueDateLessThanEqualAndProduct_Category(LocalDate dueDate, @Nullable Category category);
 }
