82578e4904800898f51a818379fb2764cae7e1ca
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-05-03 13:10:37 -0300

    Added purchaseOrderNumber to newPurchaseOrderDTO

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/purchase_order/NewPurchaseOrderDTO.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/purchase_order/NewPurchaseOrderDTO.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewPurchaseOrderDTO {
+    private Long purchaseOrderNumber;
     private BuyerDTO buyerDTO;
     private List<PurchaseOrderItems> purchaseOrderItemsList;
     private StatusOrder statusOrder = StatusOrder.CART;
