854e53f1ac8553e2c28f693454c4473cd4b1729d
Alan Siqueira <alan.siqueira@mercadolivre.com>
2022-05-03 13:12:50 -0300

    add purchaseOrderNumber to purchaseOrderDTO and its map method

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/purchase_order/PurchaseOrderDTO.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/purchase_order/PurchaseOrderDTO.java
@@ -1,5 +1,6 @@
 package com.mercadolivre.bootcamp.projeto_integrador.dto.purchase_order;
 
+import com.mercadolivre.bootcamp.projeto_integrador.entity.PurchaseOrder;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.PurchaseOrderItems;
 import com.mercadolivre.bootcamp.projeto_integrador.entity.StatusOrder;
 import lombok.*;
@@ -13,9 +14,20 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PurchaseOrderDTO {
+    private Long purchaseOrderNumber;
     private Long buyer_id;
     private List<PurchaseOrderItems> purchaseOrderItemsList;
     private StatusOrder statusOrder;
     private LocalDate purchaseOrderDate;
     private BigDecimal totalValue;
+
+    public static PurchaseOrderDTO map(PurchaseOrder purchaseOrder) {
+        return PurchaseOrderDTO.builder()
+                .purchaseOrderNumber(purchaseOrder.getPurchaseOrderNumber())
+                .buyer_id(purchaseOrder.getBuyer().getBuyerId())
+                .purchaseOrderItemsList(purchaseOrder.getPurchaseOrderItemsList())
+                .statusOrder(purchaseOrder.getStatusOrder())
+                .purchaseOrderDate(purchaseOrder.getPurchaseOrderDate())
+                .build();
+    }
 }
