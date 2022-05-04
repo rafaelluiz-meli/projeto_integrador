719919a1357ca2ff82608c155298fde3b6a1a0b7
Rafael Santana <rafael.santana@mercadolivre.com>
2022-05-04 15:00:15 -0300

    Fixing InBoundOrderController reponse

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/batch_stock/NewBatchStockDTO.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/dto/batch_stock/NewBatchStockDTO.java
@@ -16,7 +16,6 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class NewBatchStockDTO {
 
-    private Long batchNumber;
     private Integer initialQuantity;
     private Integer currentQuantity;
     private BigDecimal price;
@@ -39,7 +38,6 @@ public class NewBatchStockDTO {
 
     public static NewBatchStockDTO map(BatchStock batchStock){
         return NewBatchStockDTO.builder()
-                .batchNumber(batchStock.getBatchNumber())
                 .initialQuantity(batchStock.getInitialQuantity())
                 .currentQuantity(batchStock.getCurrentQuantity())
                 .price(batchStock.getPrice())
@@ -47,19 +45,6 @@ public class NewBatchStockDTO {
                 .manufacturingDate(batchStock.getManufacturingDate())
                 .manufacturingTime(batchStock.getManufacturingTime())
                 .product(batchStock.getProduct())
-                .build();
-    }
-
-    public BatchStock inboundMap(BatchStock currentBatchStock) {
-        return BatchStock.builder()
-                .batchNumber(currentBatchStock.getBatchNumber())
-                .initialQuantity(currentBatchStock.getInitialQuantity())
-                .currentQuantity(this.currentQuantity)
-                .price(this.price)
-                .dueDate(currentBatchStock.getDueDate())
-                .manufacturingDate(currentBatchStock.getManufacturingDate())
-                .manufacturingTime(currentBatchStock.getManufacturingTime())
-                .product(currentBatchStock.getProduct())
                 .build();
     }
 
