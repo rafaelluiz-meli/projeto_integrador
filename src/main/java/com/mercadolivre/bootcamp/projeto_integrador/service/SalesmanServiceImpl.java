364a2f8ca9e0f74f27ec95b0874f2bcb90a2d565
fgnanni <flavio.nanni@mercadolivre.com>
2022-05-05 14:24:04 -0300

    javadoc SalesmanServiceImpl

--- src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/SalesmanServiceImpl.java
+++ src/main/java/com/mercadolivre/bootcamp/projeto_integrador/service/SalesmanServiceImpl.java
@@ -8,24 +8,43 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+/**
+ * This class is the service implementation of salesman entity.
+ */
 @AllArgsConstructor
 @Service
 public class SalesmanServiceImpl implements SalesmanService {
 
     private final SalesmanRepository salesmanRepository;
 
+
+    /**
+     * Is persistence in database by repository
+     * @param salesman
+     * @return a salesman object
+     */
     @Override
     public Salesman createSalesman(Salesman salesman) {
         return salesmanRepository.save(salesman);
     }
 
+    /**
+     * Get section list filtered by salesmanId
+     * @param salesmanId
+     * @return a salesman object
+     * @exception IdNotFoundException if salesman id not exists.
+     */
     @Override
     public Salesman findSalesmanById(Long salesmanId) {
         return salesmanRepository.findById(salesmanId).
                 orElseThrow(() -> new IdNotFoundException(salesmanId));
     }
 
+    /**
+     * Get salesman list
+     * @return a salesman list
+     * @exception EmptyListException if salesman list is empty.
+     */
     @Override
     public List<Salesman> listSalesman() {
         List<Salesman> salesmanList = salesmanRepository.findAll();
@@ -33,11 +52,24 @@ public class SalesmanServiceImpl implements SalesmanService {
         return salesmanList;
     }
 
+    /**
+     * Check if SALESMAN id exists with {@link #findSalesmanById(Long)}  findSalesmanById} method. <br>
+     * If exists then remove the section.
+     * @param salesmanId
+     * @return void.
+     */
     @Override
     public void removeSalesman(Long salesmanId) {
         salesmanRepository.delete(findSalesmanById(salesmanId));
     }
 
+    /**
+     * Check if salesman id exists with {@link #findSalesmanById(Long)}  findSalesmanById} method. <br>
+     * If exists then update salesman attributes.
+     * @param salesman
+     * @return a updated salesman
+     *
+     */
     @Override
     public Salesman updateSalesman(Salesman salesman) {
         Salesman updatedSalesman = findSalesmanById(salesman.getId());
